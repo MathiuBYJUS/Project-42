67ce16f3e52b37dbbfd6020b09eaad054157025a
MathiuBYJUS <95060413+MathiuBYJUS@users.noreply.github.com>
2022-04-24 17:23:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function function1()
+{
+    document.getElementById("cradles.mp3").play();
+}
+
+function function2()
+{
+    document.getElementById("shape_of_you_best.mp3").play();    
+}
+
+function function3()
+{
+    document.getElementById("Taki-Taki.mp3").play();
+}
+
+function function4()
+{
+    document.getElementById("pikachu.mp3").play();
+}
+
+function function5()
+{
+    document.getElementById("iphone_7.mp3").play();
+}
